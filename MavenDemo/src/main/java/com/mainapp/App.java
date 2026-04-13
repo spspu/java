7f78909e8302360043ac846494c3ff75e29d6f1e
suraj <suraj@LAPTOP-7PL1H9TQ>
2026-04-13 12:54:56 +0530

    sps: added mavenDemo

---
+++ MavenDemo/src/main/java/com/mainapp/App.java
@@ -0,0 +1,13 @@
+package com.mainapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
